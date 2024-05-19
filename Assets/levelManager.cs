eca3353b0acde883c6cc75c38d195f8025ca1075
Pulkit Garg <pulkitgarg784@gmail.com>
2024-05-18 23:50:56 -0400

    Added skies

--- Assets/levelManager.cs
+++ Assets/levelManager.cs
@@ -12,10 +12,16 @@ public class levelManager : MonoBehaviour
     public TMP_Text jsonOutput;
 
     public GameObject nearStuff;
+    
     public GameObject levelPrefab;
+    
     public GameObject spikePrefab;
     public GameObject turretPrefab;
+    public GameObject platformPrefab;
 
+    public GameObject sky1Prefab;
+    public GameObject sky2Prefab;
+    public GameObject sky3Prefab;
     void SpawnObjectsFromData(string data)
     {
         data = Regex.Unescape(data);
@@ -44,12 +50,37 @@ public class levelManager : MonoBehaviour
                 Debug.Log("Spawning Turret");
                 Instantiate(turretPrefab, position, Quaternion.identity);
             }
+            else if (type == "pl")
+            {
+                Debug.Log("Spawning Platform");
+                Instantiate(platformPrefab, position, Quaternion.identity);
+            }
+            else if (type == "s1")
+            {
+                Debug.Log("Spawning Sky1");
+                GameObject sky = Instantiate(sky1Prefab, position, Quaternion.identity);
+                sky.transform.position = new Vector3(sky.transform.position.x, sky.transform.position.y, 1);
+            }
+            else if (type == "s2")
+            {
+                Debug.Log("Spawning Sky2");
+                GameObject sky = Instantiate(sky2Prefab, position, Quaternion.identity);
+                sky.transform.position = new Vector3(sky.transform.position.x, sky.transform.position.y, 1);
+            }
+            else if (type == "s3")
+            {
+                Debug.Log("Spawning Sky3");
+                GameObject sky = Instantiate(sky3Prefab, position, Quaternion.identity);
+                sky.transform.position = new Vector3(sky.transform.position.x, sky.transform.position.y, 1);
+            }
         }
     }
 
     // Use this for initialization
     void Start()
     {
+        jsonOutput.text =
+            "[[\"s1\",0,0],[\"sp\", \"5\", \"0\"],[\"sp\", \"9\", \"-10\"],[\"sp\", \"1\", \"-10\"],[\"tu\", \"7\", \"-7.5\"],[\"tu\", \"2\", \"-1.5\"],[\"tu\", \"-4\", \"-7.5\"],[\"tu\", \"13.5\", \"-4.5\"],[\"pl\", \"11\", \"-2\"],[\"pl\", \"13\", \"-5\"]]";
     }
 
     public void loadLevel()
